f6754123e8e784f93ebf701aee32d666a1eb06e0
Sbingo_666 <745677058@qq.com>
2019-07-03 14:22:07 +0800

    更换WebView库

--- src/component/WebPage.js
+++ src/component/WebPage.js
@@ -1,45 +1,21 @@
 import React from 'react';
-import {WebView} from 'react-native';
+import {WebView} from 'react-native-webview';
 import ErrorView from "../widget/ErrorView";
 import LoadingView from "../widget/LoadingView";
 
 export default class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true
-        };
-    }
-
     render() {
-        if (this.state.isLoading) {
-            return <LoadingView/>;
-        }
         const {navigation} = this.props;
         const title = navigation.getParam('title', '未知标题');
         const url = navigation.getParam('url', 'https://www.wanandroid.com/');
         return <WebView source={{uri: url, method: 'GET'}}
-                        renderLoading={this.loadingView.bind(this)}
-                        renderError={this.errorView.bind(this)}
-                        onLoadEnd={this.loadFinished.bind(this)}
+                        startInLoadingState={true}
+                        renderLoading={() => <LoadingView/>}
+                        renderError={(domain, code, desc) => <ErrorView error={desc}/>}
                         javaScriptEnabled={true}
                         domStorageEnabled={true}
-                        scalesPageToFit={true}
+                        scalesPageToFit={false}
         />;
-    }
-
-    loadingView() {
-        return <LoadingView/>;
-    }
-
-    errorView(domain, code, desc) {
-        return <ErrorView error={desc}/>;
-    }
-
-    loadFinished() {
-        this.setState({
-            isLoading: false
-        })
     }
 }
