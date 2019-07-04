12a090916d543f634dda087e9a607c3b917d0fac
Sbingo_666 <745677058@qq.com>
2019-07-04 17:16:17 +0800

    获取项目列表及其文章并展示

--- src/component/ProjectPage.js
+++ src/component/ProjectPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Text} from 'react-native';
+import ArticleTabPage from "./ArticleTabPage";
 
 export default class App extends React.Component {
 
     render() {
-        return <Text>项目列表</Text>
+        return <ArticleTabPage path={'project/tree/json'}/>
     }
 }
